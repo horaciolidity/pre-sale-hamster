8f93c0f53eee87564ee02731055fd70e0cd0cf52
horacio ortiz <145947093+horaciolidity@users.noreply.github.com>
2024-07-29 20:04:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,4 +19,26 @@ document.addEventListener('DOMContentLoaded', () => {
         payAmountInput.value = '1';
         receiveAmountInput.value = '24000';
     });
+     var endDate = new Date();
+    endDate.setDate(endDate.getDate() + 15);
+
+    function updateCountdown() {
+        var now = new Date();
+        var timeRemaining = endDate - now;
+
+        if (timeRemaining <= 0) {
+            document.getElementById('countdown').innerHTML = "Presale has ended!";
+            return;
+        }
+
+        var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+
+        document.getElementById('countdown').innerHTML = days + "d " + hours + "h " + minutes + "m";
+    }
+
+    // Actualiza el contador cada minuto
+    updateCountdown();
+    setInterval(updateCountdown, 60000);
 });
